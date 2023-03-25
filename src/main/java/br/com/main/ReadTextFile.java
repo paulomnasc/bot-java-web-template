5ada9f55e1c4d59dc43fb02097c25736ae634e5a
Paulo Nascimento <paulomnasc@gmail.com>
2023-03-25 19:35:22 -0300

    faltou esses

---
+++ src/main/java/br/com/main/ReadTextFile.java
@@ -0,0 +1,33 @@
+package br.com.main;
+
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner; // Import the Scanner class to read text files
+
+public class ReadTextFile {
+	
+	public List<String> readFile(String fileName)
+	{
+		
+		List<String> lista = new ArrayList<String>();;
+		
+		try {
+		      File myObj = new File(fileName);
+		      Scanner myReader = new Scanner(myObj);
+		      while (myReader.hasNextLine()) {
+		        String data = myReader.nextLine();
+		        lista.add(data);
+		        System.out.println(data);
+		      }
+		      myReader.close();
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+			
+		return lista;
+	}
+
+}
